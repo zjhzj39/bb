96140847e8c23c68c83620b707b50ed748bcc381
zhujihua <zhujihuademo@126.com>
2018-07-04 15:35:29 +0800

    test message addd

--- src/main/java/com/travelsky/testgitMavenSimpleProject/App.java
+++ src/main/java/com/travelsky/testgitMavenSimpleProject/App.java
@@ -1,4 +1,4 @@
-package com.travelsky.testgitMavenSimpleProject;
+﻿package com.travelsky.testgitMavenSimpleProject;
 
 /**
  * Hello world!
@@ -12,6 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldnihao!你好,张静" );
+        System.out.println( "Hello Worldnihao!你好,张静,朱际华" );
     }
 }
